0bb7f8532ba6be1bb3b44d050dc813f4f5d2ba6f
flaviu-boldea <boldicu@yahoo.co.uk>
2026-01-07 12:17:09 +0000

    Extract Slot record

--- AiStilPlay1.Tests/CreateAppointmentTests.cs
+++ AiStilPlay1.Tests/CreateAppointmentTests.cs
@@ -9,10 +9,11 @@ public class CreateAppointmentTests
         var startDate = new DateTime(2024, 6, 1, 10, 0, 0);
         var endDate = startDate.AddHours(1);
         var stylistId = Guid.Parse("11111111-1111-1111-1111-111111111111");
+        var slot = new Slot(startDate, endDate, stylistId);
         var clientId = Guid.Parse("22222222-2222-2222-2222-222222222222");
 
         //Act
-        AppointmentResponse response = new CreateAppointmentCommand().Execute(startDate, endDate, stylistId, clientId);
+        AppointmentResponse response = new CreateAppointmentCommand().Execute(slot, clientId);
 
         //Assert
         Assert.NotNull(response);
@@ -25,9 +26,11 @@ public class AppointmentResponse
     public bool IsSuccess { get; set; }
 }
 
+public record Slot(DateTime StartDate, DateTime EndDate, Guid StylistId);
+
 public class CreateAppointmentCommand
 {
-    public AppointmentResponse Execute(DateTime startDate, DateTime endDate, Guid stylistId, Guid clientId)
+    public AppointmentResponse Execute(Slot slot, Guid clientId)
     {
         return new AppointmentResponse { IsSuccess = true };
     }
