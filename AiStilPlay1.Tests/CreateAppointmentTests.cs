8285aa4bc5bd59e3308c34fe8f8d4e40dbe9db84
flaviu-boldea <boldicu@yahoo.co.uk>
2026-01-07 12:01:37 +0000

    Add first test CanCreateAppointment

--- AiStilPlay1.Tests/CreateAppointmentTests.cs
+++ AiStilPlay1.Tests/CreateAppointmentTests.cs
@@ -3,8 +3,32 @@
 public class CreateAppointmentTests
 {
     [Fact]
-    public void Test1()
+    public void CanCreateAppointment()
     {
+        //Arrange
+        var startDate = new DateTime(2024, 6, 1, 10, 0, 0);
+        var endDate = startDate.AddHours(1);
+        var stylistId = Guid.Parse("11111111-1111-1111-1111-111111111111");
+        var clientId = Guid.Parse("22222222-2222-2222-2222-222222222222");
 
+        //Act
+        AppointmentResponse response = new CreateAppointmentCommand().Execute(startDate, endDate, stylistId, clientId);
+
+        //Assert
+        Assert.NotNull(response);
+        Assert.True(response.IsSuccess);        
+    }
+}
+
+public class AppointmentResponse
+{
+    public bool IsSuccess { get; set; }
+}
+
+public class CreateAppointmentCommand
+{
+    public AppointmentResponse Execute(DateTime startDate, DateTime endDate, Guid stylistId, Guid clientId)
+    {
+        return new AppointmentResponse { IsSuccess = true };
     }
 }
