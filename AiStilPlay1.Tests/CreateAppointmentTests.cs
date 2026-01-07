eb917518562bd7c7ea495f7f66f6463090ede2f0
flaviu-boldea <boldicu@yahoo.co.uk>
2026-01-07 12:36:43 +0000

    Add failed test for slot not available

--- AiStilPlay1.Tests/CreateAppointmentTests.cs
+++ AiStilPlay1.Tests/CreateAppointmentTests.cs
@@ -3,7 +3,7 @@
 public class CreateAppointmentTests
 {
     [Fact]
-    public void CanCreateAppointment()
+    public void ShouldCreateAppointmentWhenSlotAvailable()
     {
         //Arrange
         var startDate = new DateTime(2024, 6, 1, 10, 0, 0);
@@ -20,6 +20,27 @@ public class CreateAppointmentTests
         Assert.True(response.IsSuccess);     
         Assert.Equal(slot, response.Appointment.Slot);
         Assert.Equal(clientId, response.Appointment.ClientId);   
+    }
+
+    [Fact]
+    public void ShouldRejectAppointmentWhenSlotNotAvailable()
+    {
+        //Arrange
+        var startDate = new DateTime(2024, 6, 1, 10, 0, 0);
+        var endDate = startDate.AddHours(1);
+        var stylistId = Guid.Parse("11111111-1111-1111-1111-111111111111");
+        var slot = new Slot(startDate, endDate, stylistId);
+        var clientId = Guid.Parse("22222222-2222-2222-2222-222222222222");
+
+        CreateAppointmentCommand.Execute(slot, clientId);
+
+        //Act
+        AppointmentResponse response = CreateAppointmentCommand.Execute(slot, clientId);
+
+        //Assert
+        Assert.NotNull(response);
+        Assert.False(response.IsSuccess);     
+        Assert.Null(response.Appointment);
     }
 }
 
